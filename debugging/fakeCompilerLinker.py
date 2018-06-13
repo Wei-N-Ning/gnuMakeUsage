bd09d4ede2ef8d9372dbec9e212fbb666ef0d8af
Wei Ning <wning@wetafx.co.nz>
2018-06-14 11:17:18 +1200

    - update debugging/fakecc

--- debugging/fakeCompilerLinker.py
+++ debugging/fakeCompilerLinker.py
@@ -59,10 +59,9 @@ class FakeCompiler(object):
         assert self.out_path, 'missing output path'
         with open(self.out_path, 'wb') as fp:
             fp.write(self.contents)
-        if not self.to_link:
-            if self.dep_path:
-                with open(self.dep_path, 'w') as fp:
-                    fp.write(self.dep_contents)
+        if self.dep_path:
+            with open(self.dep_path, 'w') as fp:
+                fp.write(self.dep_contents)
         self.logger.log_args()
 
 
@@ -81,3 +80,67 @@ class FakeLinker(object):
         with open(self.out_path, 'wb') as fp:
             fp.write(self.contents)
         self.logger.log_args()
+
+
+class GetIncludeDirs(object):
+    
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self._paths = set()
+
+    def _do(self, path):
+        if path in self._paths:
+            return
+        if path.starswith('/vol/bob/check'):
+            return
+        if path.startswith('/vol/bob/built/linux64/L48/dev'):
+            return
+        self._paths.add(path)
+
+    def do(self):
+        with open(self.file_path, 'r') as fp:
+            for each_line in fp:
+                for arg in each_line.split(' '):
+                    if arg.startswith('-I'):
+                        self._do(arg[2:])
+        return list(self._paths)
+
+
+class GetDFlags(object):
+    
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self._flags = set()
+
+    def _do(self, flag):
+        key, value = (flag.split('=') + [''])[0:2]
+        if value:
+            if len(value) > 20:
+                return
+            elif value.count('"') == 1:
+                value = "'{}'".format(value.replace('"', ''))
+            elif value.count('"') == 2:
+                value = value.replace('"', '\'')
+            elif not value.isdigit():
+                value = '\'{}\''.format(value)
+            flag = '"{}={}"'.format(key, value)
+        else:
+            flag = '"{}"'.format(key)
+        if flag in self._flags:
+            return
+        self._flags.add(flag)
+
+    def do(self):
+        with open(self.file_path, 'r') as fp:
+            for each_line in fp:
+                for arg in each_line.split(' '):
+                    if arg.startswith('-D'):
+                        self._do(arg[2:])
+        return list(self._flags)
+
+
+
+
+
+
+
