e95668acb3de13e23ad8a9e5862c98c735d29566
Wei Ning <wning@wetafx.co.nz>
2018-06-26 10:30:25 +1200

    - update find program ver example

--- fileSystem/findProgramVersion.mk
+++ fileSystem/findProgramVersion.mk
@@ -1,5 +1,8 @@
-GVER := "$(shell gcc --version | grep -Po '\d\.\d.\d+' | sort -u)"
+GVER := $(shell gcc --version | grep -Po '\d\.\d.\d+' | sort -u)
 
+ifneq "$(GVER)" "5.0.0"
+$(error version is not 5.0.0, current: $(GVER))
+endif
 
 .PHONY: __
 __:
