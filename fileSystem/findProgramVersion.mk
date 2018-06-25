40f39610344e0b9c9d9f253bc393e528d00d80d7
Wei Ning <wning@wetafx.co.nz>
2018-06-26 11:08:00 +1200

    - update find program version, showing even more complex piping

--- fileSystem/findProgramVersion.mk
+++ fileSystem/findProgramVersion.mk
@@ -1,10 +1,11 @@
-GVER := $(shell gcc --version | grep -Po '\d\.\d.\d+' | sort -u)
+GCC_MAJOR_MINOR_VER := $(shell gcc --version | grep -Po '\d\.\d.\d+' | sort -u | grep -Po '^\d\.\d')
 
-ifneq "$(GVER)" "5.0.0"
-$(error version is not 5.0.0, current: $(GVER))
+
+ifneq "$(GCC_MAJOR_MINOR_VER)" "5.0"
+$(error version is not 5.0.x, current: $(GCC_MAJOR_MINOR_VER))
 endif
 
 .PHONY: __
 __:
-	@echo $(GVER)
+	@echo passed
 
