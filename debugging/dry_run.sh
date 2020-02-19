2ca19458d274d7942445bed765dbb1fdc1aa4add
Wei Ning <wei.ning@digitalasset.com>
2020-02-19 13:09:36 +1100

    debugging

---
+++ debugging/dry_run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# source:
+# https://www.gnu.org/software/automake/manual/html_node/Debugging-Make-Rules.html
+# make -n can help show what would be done without actually doing it
+
+make -n run-ft
